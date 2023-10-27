7eb8cb3bc51cc194fb2ab4d4cb465deb44eb98ee
sergiomanresa <sergiojmb8@gmail.com>
2023-10-27 19:59:47 +0200

    finalizacion del proyecto supongo :)

--- app/src/main/java/com/example/dronetech/Cuadricopterobinario.java
+++ app/src/main/java/com/example/dronetech/Cuadricopterobinario.java
@@ -10,6 +10,6 @@ public class Cuadricopterobinario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_drones);
+        setContentView(R.layout.cuadricoptero);
     }
 }
